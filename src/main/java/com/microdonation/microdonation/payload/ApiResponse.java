362624a47bbdce4942aa7984591f77021462a2cc
Tanuj Tandon <tanuj.tandon@hcl.com>
2020-07-12 00:46:33 +0530

    Changes reverted for Exception Handling

--- src/main/java/com/microdonation/microdonation/payload/ApiResponse.java
+++ src/main/java/com/microdonation/microdonation/payload/ApiResponse.java
@@ -3,9 +3,7 @@ package com.microdonation.microdonation.payload;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.io.Serializable;
-import java.util.List;
 
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -13,14 +11,7 @@ public class ApiResponse<T extends Object> implements Serializable {
     private Boolean success;
     private String message;
     private T data;
-    private String error;
 
-    public String getError() {
-		return error;
-	}
-	public void setError(String error) {
-		this.error = error;
-	}
 	public ApiResponse(Boolean success, String message) {
         this.success = success;
         this.message = message;
@@ -36,7 +27,7 @@ public class ApiResponse<T extends Object> implements Serializable {
     private ApiResponse(ResponseBuilder builder) {
         this.success = builder.success;
         this.message = builder.message;
-        this.error = builder.error;
+        this.data = builder.data;
     }
 
     public Boolean getSuccess() {
@@ -63,26 +54,27 @@ public class ApiResponse<T extends Object> implements Serializable {
         this.data = data;
     }
 
-    public static class ResponseBuilder {
+    public  class ResponseBuilder {
         @JsonProperty(value = "success")
         private boolean success;
         @JsonProperty(value = "message")
         private String message;
-        @JsonProperty(value = "error")
-        private String error;
+        @JsonProperty(value = "data")
+        private T data;
 
-		public ResponseBuilder setError(String error) {
-			this.error = error;
-			return this;
-		}
-
-		public ResponseBuilder setSuccess(boolean success) {
+        public ResponseBuilder setSuccess(boolean success) {
             this.success = success;
             return this;
         }
 
+
         public ResponseBuilder setMessage(String message) {
             this.message = message;
+            return this;
+        }
+
+        public ResponseBuilder setData(T data ) {
+            this.data = data;
             return this;
         }
 
